3ba922d06d94f33fa6bad081b14e8d346dbf33f7
YaSmin227 <yasmintolba227@gimal.com>
2022-07-07 15:59:43 +0200

    course_details

--- new_assets/js/main.js
+++ new_assets/js/main.js
@@ -1,1 +1,8 @@
-
+// change collapse icon
+function test_collapse(that) {
+    if ($(that).attr("aria-expanded") === "true") {
+        $(that).find('.bi-caret-left-fill').toggleClass('bi-caret-left-fill  bi-caret-down-fill')
+    } else {
+        $(that).find('.bi-caret-down-fill').toggleClass('bi-caret-down-fill  bi-caret-left-fill')
+    }
+}
