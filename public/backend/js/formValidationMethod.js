287fbd630261ce2afad77f8942f1c27f1b755dc3
midoyousef <muhammadmedhat12@gmail.com>
2022-12-26 14:16:21 +0200

    form valdiation

--- public/backend/js/formValidationMethod.js
+++ public/backend/js/formValidationMethod.js
@@ -1,13 +1,18 @@
 function formValidateRequest(data, form = 'none') {
   // console.log($(form).find("input[name=open_time]").attr('name'));
-  console.log(form);
+  // console.log(form);
   try {
     let messages = data.error_messages;
     if (messages.length > 0) {
       messages.forEach(function (message) {
         let name = Object.keys(message)[0];
         $(`[id=${name}-error]`).remove();
-        let custom_label = $(`[name=${name}]`, form).closest('div');
+        let custom_label = "";
+        if (form != 'none') {
+          custom_label = $(`[name=${name}]`, form).closest('div');
+        } else {
+          custom_label = $(`[name=${name}]`).closest('div');
+        }
         let validate_show = `<small
                                           class="error-message text-danger position-relative"
                                           style="text-transform: capitalize; font-weight: bold">
