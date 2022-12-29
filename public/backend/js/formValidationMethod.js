3ca7c29137db22480368a2281e7009d110f4cf1d
Ahmed Mustafa <ahmedellnemr@gmail>
2022-12-29 11:39:49 +0200

    validatin span

--- public/backend/js/formValidationMethod.js
+++ public/backend/js/formValidationMethod.js
@@ -1,29 +1,23 @@
 function formValidateRequest(data, form = 'none') {
   // console.log($(form).find("input[name=open_time]").attr('name'));
-  // console.log(form);
+   console.log(form);
   try {
     let messages = data.error_messages;
     if (messages.length > 0) {
       messages.forEach(function (message) {
         let name = Object.keys(message)[0];
         $(`[id=${name}-error]`).remove();
-        let custom_label = "";
+        let elemmentObj = "";
         if (form != 'none') {
-          custom_label = $(`[name=${name}]`, form).closest('div');
+          elemmentObj = $(`[name=${name}]`, form);
         } else {
-          custom_label = $(`[name=${name}]`).closest('div');
+          elemmentObj = $(`[name=${name}]`);
         }
-        //console.log(custom_label)
         let validate_show =`<span class="help-block form-error"> <i class="fa fa-exclamation-circle" aria-hidden="true"></i> ${message[name] ?? 'This field is required '}</span>`;
-        /*let validate_show = `<small
-                                          class="error-message text-danger position-relative"
-                                          style="text-transform: capitalize; font-weight: bold">
-                                          <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
-                                          ${message[name] ?? 'This field is required '}
-                                      </small>`;*/
-        custom_label.children('input').after(validate_show);
-        custom_label.children('input').addClass('red-border');
-        custom_label.children('.nice-select').addClass('red-border');
+        console.log(elemmentObj)
+        elemmentObj.closest("div").find("span.form-error").remove();
+        elemmentObj.addClass('red-border');
+        elemmentObj.after(validate_show);
       });
     }
   } catch (e) {
